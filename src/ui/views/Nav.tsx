4699b5be383fbd3d7e7d951360408534c1a3918b
Andrei Stepanenko <ftvkyo@gmail.com>
2023-08-01 21:53:12 +0100

    Add filtering by sections after filtering by series

--- src/ui/views/Nav.tsx
+++ src/ui/views/Nav.tsx
@@ -49,27 +49,88 @@ function acquireSeries(notes: DataArray<Record<string, any>>) {
 }
 
 
-function generateNoteCards(
-    currentSeries: string,
+function filterNotesBySeries(
     notes: DataArray<Record<string, any>>,
-    seriesWithout: DataArray<Record<string, any>>,
-    openNote: (e: React.MouseEvent<HTMLAnchorElement>) => Promise<void>
+    series: string,
+    withoutSeries: DataArray<Record<string, any>>,
 ) {
-    const any = currentSeries === "*";
-    const withoutSeries = currentSeries === "!";
+    const includeAnySeries = series === "*";
+    const onlyWithoutSeries = series === "!";
 
-    const notesFiltered = any ? notes
-        : withoutSeries ? seriesWithout
-            : notes.filter(page => page.file.tags.includes(currentSeries));
+    const notesFiltered = includeAnySeries ? notes
+        : onlyWithoutSeries ? withoutSeries
+        : notes.filter(page => page.file.tags.includes(series));
 
-    const noteCards = notesFiltered
+    return notesFiltered;
+}
+
+
+function acquireSections(notes: DataArray<Record<string, any>>) {
+    // Currently note sections are determined by note path.
+
+    const sections: Record<string /* key */, number> = {};
+
+    for (const note of notes) {
+        const path = note.file.path;
+
+        const parts = path.split("/");
+
+        // Expecting the parts to be:
+        // 0. Root note directory
+        // 1. Section
+        // 2. Year
+        // 3. Note identifier (filename)
+
+        // Extract the section:
+        const section = parts[1];
+
+        // Count the number of notes in each section.
+        const count = sections[section] ?? 0;
+        sections[section] = count + 1;
+    }
+
+    // Sort the sections by name.
+    const sectionsAbc: [string, string][] = Object.entries(sections)
+        .map(([key, value]) => {
+            const record: [string, string] = [key, `${key} (${value})`];
+            return record;
+        })
+        .sort((a, b) => a[0].localeCompare(b[0]));
+
+    // Add an "All" option.
+    sectionsAbc.unshift(["*", `All (${notes.length})`]);
+
+    return sectionsAbc;
+}
+
+
+function filterNotesBySection(
+    notes: DataArray<Record<string, any>>,
+    section: string,
+) {
+    const includeAnySection = section === "*";
+
+    return includeAnySection
+        ? notes
+        : notes.filter(page => {
+            const path = page.file.path;
+            const parts = path.split("/");
+            const noteSection = parts[1];
+            return noteSection === section;
+        });
+}
+
+
+function generateNoteCards(
+    notes: DataArray<Record<string, any>>,
+    openNote: (e: React.MouseEvent<HTMLAnchorElement>) => Promise<void>,
+) {
+    return notes
         .map(page => <NoteCard
             key={page.file.name}
             filename={page.file.name}
             openNoteCallback={openNote} />
         );
-
-    return noteCards;
 }
 
 
@@ -88,11 +149,17 @@ const NavView: ViewElement = {
         const {dv, notesSource} = plugin;
 
         const [currentSeries, setCurrentSeries] = useState<string>("*" /* all */);
+        const [currentSection, setCurrentSection] = useState<string>("*" /* all */);
+
+        // Create a universal note opener to avoid making one for every note.
+        const openNote = useCallback(openNoteCallback.bind(plugin), []);
 
         const notes = dv.pages(notesSource);
 
         // Acquire series tags for building a selector.
         const {seriesAbc, seriesWithout} = acquireSeries(notes);
+
+        const currentSeriesNotes = filterNotesBySeries(notes, currentSeries, seriesWithout);
 
         // Create a series selector.
         const seriesSelector = <Selector
@@ -102,14 +169,27 @@ const NavView: ViewElement = {
             onChange={setCurrentSeries}
         />;
 
-        // Create a universal note opener to avoid making one for every note.
-        const openNote = useCallback(openNoteCallback.bind(plugin), []);
+        // Acquire sections based on current series so we get nice numbers.
+        const sectionsAbc = acquireSections(currentSeriesNotes);
+        const currentSectionNotes = filterNotesBySection(currentSeriesNotes, currentSection);
 
-        const noteCards = generateNoteCards(currentSeries, notes, seriesWithout, openNote);
+        // Create a section selector.
+        const sectionSelector = <Selector
+            label="Section"
+            options={sectionsAbc}
+            value={currentSection}
+            onChange={setCurrentSection}
+        />;
+
+        const noteCards = generateNoteCards(
+            currentSectionNotes,
+            openNote,
+        );
 
         return <>
             <div className="filters">
                 {seriesSelector}
+                {sectionSelector}
             </div>
             <div className="results">
                 {noteCards}
