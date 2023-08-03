292584adf7d25d3d1bc23647dfc813670d7558fd
Andrei Stepanenko <ftvkyo@gmail.com>
2023-08-03 16:37:39 +0100

    Separate NoteCard info and controls

--- src/ui/components/NoteCard.tsx
+++ src/ui/components/NoteCard.tsx
@@ -1,19 +1,25 @@
 import ApiNote from "@/api/note";
+import {setIcon} from "obsidian";
+import {useEffect, useRef} from "react";
 
 
 function open(
     e: React.MouseEvent<HTMLAnchorElement>,
 ) {
     e.preventDefault();
+
     const href = e.currentTarget.getAttribute("href");
     if (!href) {
         return;
     }
+
     const note = ApiNote.fromPath(href);
     if (!note) {
         return;
     }
-    note.reveal();
+
+    const newTab = e.currentTarget.getAttribute("target") === "_blank";
+    note.reveal({ replace: !newTab });
 }
 
 
@@ -22,30 +28,59 @@ export default function NoteCard({
 }: {
     note: ApiNote,
 }) {
+    const openReplaceRef = useRef<HTMLAnchorElement>(null);
+    const openReplace = <a
+        ref={openReplaceRef}
+        className="internal-link note-opener"
+        href={note.path}
+        onClick={open}
+    />;
+
+    const openNewTabRef = useRef<HTMLAnchorElement>(null);
+    const openNewTab = <a
+        ref={openNewTabRef}
+        className="internal-link note-opener"
+        href={note.path}
+        onClick={open}
+        target="_blank"
+    />;
+
+    useEffect(() => {
+        if (openReplaceRef.current) {
+            setIcon(openReplaceRef.current, "corner-down-right");
+        }
+        if (openNewTabRef.current) {
+            setIcon(openNewTabRef.current, "plus");
+        }
+    }, [openNewTabRef, openReplaceRef]);
+
     // The title used is either:
     // 1. The note's h1, when available
     // 2. The note's date parsed from the basename, when available
     // 3. The note's basename, when all else fails
-    const title = note.h1 ?? note.dateInfo ?? note.base;
+    const blockTitle = <p className="title">
+        {note.h1 ?? note.dateInfo ?? note.base}
+    </p>
 
-    // The label/tooltip used shows the note's filename.
-    const label = `Id: ${note.base}`;
+    const blockDate = note.h1 && note.type === null
+        ? <code className="date">{note.dateInfo}</code>
+        : null;
 
-    return <a
-        className="note-card internal-link"
-        href={note.path}
-        onClick={open}
+    return <div
+        className="note-card"
 
         // Define the tooltip and accesibility label.
-        aria-label={label}
+        aria-label={note.base}
         data-tooltip-position="right"
-
-        // Just copying obsidian here (probably)
-        data-href={note.path}
-
-        target="_blank"
-        rel="noopener noreferrer"
     >
-        <p>{title}</p>
-    </a>;
+        <div className="info">
+            {blockTitle}
+            {blockDate}
+        </div>
+
+        <div className="controls">
+            {openReplace}
+            {openNewTab}
+        </div>
+    </div>
 }
