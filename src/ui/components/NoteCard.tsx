bc94e5ed3b95a6598eb3695f13f014aff6228ad7
Andrei Stepanenko <ftvkyo@gmail.com>
2023-08-03 12:51:14 +0100

    NoteCard display date info when no title is available

--- src/ui/components/NoteCard.tsx
+++ src/ui/components/NoteCard.tsx
@@ -11,9 +11,17 @@ export default function NoteCard({
     openNoteCallback: (e: React.MouseEvent<HTMLAnchorElement>) => Promise<void>,
 }) {
     const plugin = usePlugin();
-    const title = plugin.api.note.getTitle(filename) ?? filename;
 
-    const date = useMemo(() => plugin.api.note.getDateInfo(filename) ?? "", [filename]);
+    const dateInfo = useMemo(() => plugin.api.note.getDateInfo(filename), [filename]);
+
+    // The title used is either:
+    // 1. The note's title, when available
+    // 2. The note's date parsed from the filename, when available
+    // 3. The note's filename, when all else fails
+    const title = plugin.api.note.getTitle(filename) ?? dateInfo ?? filename;
+
+    // The label/tooltip used shows the note's filename.
+    const label = `Id: ${filename}`;
 
     return <a
         className="note-card internal-link"
@@ -21,7 +29,7 @@ export default function NoteCard({
         onClick={openNoteCallback}
 
         // Define the tooltip and accesibility label.
-        aria-label={date}
+        aria-label={label}
         data-tooltip-position="right"
 
         // Just copying obsidian here (probably)
