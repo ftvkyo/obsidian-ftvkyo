2858eef9099bff100de1ffc98b6a27cc51f015fc
Andrei Stepanenko <ftvkyo@gmail.com>
2023-09-26 20:06:30 +0100

    Delete unnecessary file

--- src/ui/views/nav_todo.ts
+++
@@ -1,8 +0,0 @@
-export const u = undefined;
-
-//const specialTags = {
-//    "#draft": "Drafts",
-//    "#public": "Publications",
-//    "#fixme": "Fix these",
-//    "#le": "Loose ends (TODO collect explanation)",
-//};
