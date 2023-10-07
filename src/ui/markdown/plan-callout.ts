f500bc170626fe79c5ff8a74c558bbffca0dd9af
Andrei Stepanenko <ftvkyo@gmail.com>
2023-10-07 16:32:49 +0100

    Add projected finishing time

--- src/ui/markdown/plan-callout.ts
+++ src/ui/markdown/plan-callout.ts
@@ -9,6 +9,7 @@ let lg: Logger | undefined = undefined;
 
 
 const BREAK_MINUTES = 5;
+const RE_TIME_MOMENT = /^@\s?(\d\d?):(\d\d)$/;
 const RE_TIME_DELTA = /^(\d+[hH])?\s?(\d+[mM])?$/;
 
 
@@ -72,11 +73,49 @@ class PlanCalloutCalculator extends MarkdownRenderChild {
         return ret;
     }
 
+    #formatEndTime(start: {h: number, m: number}, minutes: number): string {
+        start.m += minutes;
+        start.h += Math.floor(start.m / 60);
+        start.m %= 60;
+
+        const days = Math.floor(start.h / 24);
+        start.h %= 24;
+
+        const mpad = String(start.m).padStart(2, "0");
+
+        let ret = `@ ${start.h}:${mpad}`;
+        if (days > 0) {
+            ret += ` (+${days}d)`;
+        }
+
+        return ret;
+    }
+
     #getListItems(): HTMLLIElement[] {
         return Array.from(this.containerEl.querySelectorAll("li"));
     }
 
+    #getStartTime(): null | {h: number, m: number} {
+        const code = this.containerEl.querySelector<HTMLElement>(".callout-title-inner > code:first-of-type");
+        if (!code) {
+            lg?.debug("Code in title not found.");
+            return null;
+        }
+
+        const match = RE_TIME_MOMENT.exec(code.innerText) ?? [];
+        const [, h, m] = match;
+
+        if (h === undefined || m === undefined) {
+            lg?.debug(`Could not extract start time from ${code.innerText}`);
+            return null;
+        }
+
+        return {h: Number(h), m: Number(m)};
+    }
+
     onload() {
+        const start = this.#getStartTime();
+
         let minutesTotal = 0;
         let breaks = 0;
 
@@ -89,19 +128,30 @@ class PlanCalloutCalculator extends MarkdownRenderChild {
                 minutesTotal += minutes + BREAK_MINUTES;
                 breaks++;
             }
-            // TODO: add a marker for successfully parsed lis
         }
 
         lg?.debug(`Total minutes is ${minutesTotal}.`);
 
         if (minutesTotal > 0) {
+            const em = document.createElement("em");
+
+            em.appendText("Total: ");
+            const codeTotal = document.createElement("code");
             const td = this.#formatTimeDelta(minutesTotal);
+            codeTotal.innerText = td;
+            em.appendChild(codeTotal);
             const brs = this.#formatBreaks(breaks);
+            em.appendText(`, ${brs}.`);
 
-            const p = document.createElement<"em">("em");
-            p.innerText = `Total: ${td}, ${brs}.`;
+            if (start) {
+                em.appendText(" Finishing: ");
+                const codeEnd = document.createElement("code");
+                codeEnd.innerText = this.#formatEndTime(start, minutesTotal);
+                em.appendChild(codeEnd);
+                em.appendText(".");
+            }
 
-            this.containerEl.appendChild(p);
+            this.containerEl.appendChild(em);
         }
     }
 }
