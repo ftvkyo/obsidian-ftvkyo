a492e79573292808f1b8270049db9b28bfcf4c72
Andrei Stepanenko <ftvkyo@gmail.com>
2023-10-10 15:54:56 +0100

    Extract TagCard from TagList

--- src/ui/components/TagList.tsx
+++ src/ui/components/TagList.tsx
@@ -1,7 +1,27 @@
-import ApiNoteList, { tagDisplay, TagWildcard } from "@/api/note-list";
 import { clsx } from "clsx";
 import { useMemo } from "react";
+
+import ApiNoteList, { tagDisplay, TagWildcard } from "@/api/note-list";
+
 import styles from "./TagList.module.scss";
+
+
+function TagCard({
+    id,
+    onClick,
+}: {
+    id: string | TagWildcard,
+    onClick: (e: React.MouseEvent<HTMLDivElement>) => void,
+}) {
+    return <div
+        key={id}
+        data-id={id}
+        className={clsx(styles.card)}
+        onClick={onClick}
+    >
+        {tagDisplay(id)}
+    </div>
+}
 
 
 export default function TagList({
@@ -11,6 +31,12 @@ export default function TagList({
     notes: ApiNoteList,
     setTag: (id: string | TagWildcard) => void,
 }) {
+    const wildcards = [
+        TagWildcard.All,
+        TagWildcard.Any,
+        TagWildcard.None,
+    ];
+
     const onClick = useMemo(() => (e: React.MouseEvent<HTMLDivElement>) => {
         const id = e.currentTarget.getAttribute("data-id");
         if (id) {
@@ -19,21 +45,14 @@ export default function TagList({
     }, [setTag]);
 
     return <div className={styles.list}>
-        {[TagWildcard.All, TagWildcard.Any, TagWildcard.None].map((w) => <div
-            key={w}
-            className={styles.card}
+        {wildcards.map((w) => <TagCard
+            id={w}
             onClick={() => setTag(w)}
-        >
-            {tagDisplay(w)}
-        </div>)}
+        />)}
 
-        {notes.tags.map((id) => <div
-            key={id}
-            data-id={id}
-            className={clsx(styles.card, styles.tag)}
+        {notes.tags.map((id) => <TagCard
+            id={id}
             onClick={onClick}
-        >
-            {tagDisplay(id)}
-        </div>)}
+        />)}
     </div>
 }
