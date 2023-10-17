4588e8fc4e2b6ee48d14ef16ab108d005659f9ef
Andrei Stepanenko <ftvkyo@gmail.com>
2023-10-17 16:53:33 +0100

    Actually find the periodic notes

--- src/api/source.ts
+++ src/api/source.ts
@@ -16,11 +16,19 @@ export default class ApiSource {
     }
 
     update() {
+        // TODO: Also check that the filename pattern matches?
+        // TODO: Make sure the paths are handled correctly (`abc-def/` should not be included when looking for `abc/`)
+
         const mdfs = ftvkyo.app.vault.getMarkdownFiles();
 
         const unique = mdfs.filter((v) => v.path.startsWith(ftvkyo.deps.unique.options.folder))
 
         const periodic = mdfs.filter((v) => {
+            for (const p of Object.values(ftvkyo.deps.periodic.settings)) {
+                if (p.enabled && v.path.startsWith(p.folder)) {
+                    return true;
+                }
+            }
             return false;
         });
 
