ad6ae60a61a56ef7d7febef01bb5498fbe0d91dd
Andrei Stepanenko <ftvkyo@gmail.com>
2023-11-08 22:42:18 +0000

    Improve template engine code and approach

--- src/util/templates.ts
+++ src/util/templates.ts
@@ -1,8 +1,9 @@
 import { NoteType } from "@/api/source";
 import { TFile } from "obsidian";
+import { equalUpTo } from "./date";
 
 
-const RE_TEMPLATE = /{{(?<what>.+?):(?<fmt>.+?)(?<mod>:.+?)?}}/gm;
+const RE_TEMPLATE = /{{(?<what>.+?):(?<fmt>.+?)(?<flags>:.+?)?}}/gm;
 
 
 /* ========== *
@@ -14,7 +15,9 @@ export async function replaceTemplates(noteType: NoteType, date: moment.Moment, 
     const content = await app.vault.read(note);
 
     // 2. Find and replace all the templates in the file
-    const newContent = content.replace(RE_TEMPLATE, (_, what, fmt, mod) => fmtTemplate(noteType, date, what, fmt, mod));
+    const newContent = content.replace(RE_TEMPLATE, (_, what: string, fmt: string, flags: string | undefined) => {
+        return fmtTemplate(noteType, date, what, fmt, flags ? flags.substring(1).split(",") : []);
+    });
 
     // 3. Update the file with the new content
     await app.vault.modify(note, newContent);
@@ -25,7 +28,13 @@ export async function replaceTemplates(noteType: NoteType, date: moment.Moment, 
  * Types *
  * ===== */
 
-type Formatter = (date: moment.Moment, fmt: string, mod?: string) => string;
+type Formatter = (date: moment.Moment, fmt: string, flags: string[]) => string;
+
+type FormatterDecorator = (f: Formatter) => Formatter;
+
+type DateMultiplicator = (date: moment.Moment) => moment.Moment[];
+
+type DateReducer = (entries: string[]) => string;
 
 type NoteFormatters = {
     [key: string]: Formatter,
@@ -37,38 +46,63 @@ type FormatConfig = {
 
 
 /* ==================== *
+ * Formatter decorators *
+ * ==================== */
+
+const maybeLinks: FormatterDecorator = f => (date, fmt, flags) => {
+    const text = f(date, fmt, flags)
+    if (!flags.includes("link")) {
+        return text;
+    }
+
+    return `[[${text}]]`;
+};
+
+const list: DateReducer = entries => {
+    return entries.map(e => `- ${e}\n`).join("");
+};
+
+const enumerate: DateReducer = entries => {
+    return entries.join(", ");
+};
+
+
+/* =================== *
+ * Date multiplicators *
+ * =================== */
+
+const eachWeekInMonth: DateMultiplicator = (date) => {
+    const day = date.clone().date(1);
+    const weeks: moment.Moment[] = [];
+    while (equalUpTo(day, date, "month")) {
+        const weekExists = weeks.find(w => equalUpTo(w, day, "week"));
+        if (!weekExists) {
+            weeks.push(day.clone());
+        }
+        day.add(1, "day");
+    }
+    return weeks;
+}
+
+/* ==================== *
  * Available formatters *
  * ==================== */
 
-const fmtIdentity: Formatter = (date, fmt) => date.format(fmt);
+const fmtIdentity: Formatter = maybeLinks((date, fmt) => date.format(fmt));
 
 // Days in week start at 0
-const fmtWeekday: (day: 0 | 1 | 2 | 3 | 4 | 5 | 6) => Formatter = (day) => (date, fmt) => date.weekday(day).format(fmt);
+const fmtWeekday: (day: 0 | 1 | 2 | 3 | 4 | 5 | 6) => Formatter = (day) => maybeLinks((date, fmt) => date.weekday(day).format(fmt));
 
-const fmtAllWeeksInMonth: Formatter = (date, fmt, mod) => {
-    const mods = {
-        "linklist": ["- [[", "\n", "]]"],
-        "linkenum": ["[[", ", ", "]]"],
-    } as const;
+const fmtWeeksInMonth: Formatter = (date, fmt, flags) => {
+    const weeks = eachWeekInMonth(date);
+    const entries = weeks.map(w => fmtIdentity(w, fmt, flags));
 
-    if (!mod || !(mod in mods)) {
-        return `\`unknown mod '${mod}' (available ${Object.keys(mods)})\``;
+    if (flags.includes("list")) {
+        return list(entries);
     }
 
-    const [prefix, separator, suffix] = mods[mod as keyof typeof mods];
-
-    const month = date.month();
-    const weeks: string[] = [];
-    date.date(1);
-    while (date.month() === month) {
-        const dateText = date.format(fmt);
-        if (!weeks.includes(dateText)) {
-            weeks.push(dateText);
-        }
-        date.add(1, "day");
-    }
-    return weeks.map(weekText => prefix + weekText + suffix).join(separator);
-};
+    return enumerate(entries);
+}
 
 const FMT: FormatConfig = {
     unique: {
@@ -87,7 +121,7 @@ const FMT: FormatConfig = {
         // Months have variable lengths, so go to the next month and underflow.
         last: (date, fmt) => date.add(1, "month").date(0).format(fmt),
         // Magic thing to format every week in the month and join them
-        "week*": fmtAllWeeksInMonth,
+        "weeks": fmtWeeksInMonth,
     },
     week: {
         date: fmtIdentity,
@@ -105,11 +139,11 @@ const FMT: FormatConfig = {
 };
 
 
-function fmtTemplate(noteType: NoteType, date: moment.Moment, what: string, fmt: string, mod?: string): string {
+function fmtTemplate(noteType: NoteType, date: moment.Moment, what: string, fmt: string, flags: string[]): string {
     const noteFmts = FMT[noteType];
     const whatFmt = noteFmts[what]
     if (whatFmt) {
-        return whatFmt(date.clone(), fmt, mod?.substring(1));
+        return whatFmt(date.clone(), fmt, flags);
     }
     return `\`unknown formatter '${what}'\``;
 }
