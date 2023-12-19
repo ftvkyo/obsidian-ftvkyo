657ea31caa6cd8f7ca4dc7ebfd63d76645ef795c
Andrei Stepanenko <ftvkyo@gmail.com>
2023-12-19 15:47:07 +0000

    feat(Explore): remove the tag tree and note list

--- src/ui/views/Explore.tsx
+++ src/ui/views/Explore.tsx
@@ -1,10 +1,5 @@
-import {useState} from "react";
-
 import {type ViewElement} from "./view";
 import Logger from "@/util/logger";
-import NoteList from "../components/NoteList";
-import { Tag } from "@/api/note-list";
-import TagList from "../components/TagList";
 import Calendar from "../components/Calendar";
 
 
@@ -17,25 +12,13 @@ const ExploreView: ViewElement = {
             lg = ftvkyo.lg.sub("Explore");
         }
 
-        const [tag, setTag] = useState<Tag | null>(null);
-
-        const unique = ftvkyo.api.source.cache.unique;
+        // const unique = ftvkyo.api.source.cache.unique;
         const periodic = ftvkyo.api.source.cache.periodic;
 
         return <>
             <Calendar
                 notes={periodic}
             />
-            {tag
-            ? <NoteList
-                tag={tag}
-                goBack={() => setTag(null)}
-                notes={unique}
-            />
-            : <TagList
-                setTag={setTag}
-                notes={unique}
-            />}
         </>;
     },
     short: "nav",
